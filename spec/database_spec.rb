6664123a538bb729f66ff49aed1e1c3447f388c1
Chetan Sanghani <crsanghani@gmail.com>
2016-11-28 13:14:36 +0000

    test written to check that values are being set

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 feature "Accessing the server" do
 
-  scenario 'server is accessible on a specific port' do
+  scenario "server is accessible on a specific port" do
     visit 'http://localhost:4000'
     expect(page).to have_content "this proves the homepage is on the custom port"
   end
 
+end
+
+feature "storing temporary variables" do
+
+  scenario "setting values" do
+    visit '/set?somekey=somevalue'
+    expect(page).to have_content 'somekey: somevalue'
+  end
 end
