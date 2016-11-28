76052dcdfd884a532626623d9dd6230d34d912e7
Chetan Sanghani <crsanghani@gmail.com>
2016-11-28 13:24:14 +0000

    tests passing

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -2,9 +2,17 @@ require 'sinatra/base'
 
 class MyApp < Sinatra::Base
   set :port, 4000
+  enable :sessions
 
   get '/' do
     'this proves the homepage is on the custom port'
+  end
+
+  get '/set' do
+    key = params.flatten[-2]
+    value = params.flatten[-1]
+    session[key] = value
+    "key value page is #{key}: #{value}"
   end
 
   # start the server if ruby file executed directly
